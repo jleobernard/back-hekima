396ae8463a6400da9f20f47069aef59f55fd7940
junior bernard <junior.bernard-ext@alantaya.com>
2021-06-21 23:11:39 +0200

    Cache des images

---
+++ src/main/java/com/leo/hekima/web/CacheFilter.java
@@ -0,0 +1,21 @@
+package com.leo.hekima.web;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CacheFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        var path = serverWebExchange.getRequest().getPath().toString();
+        if(path.matches("/api/notes/[0-9a-z]+/files/[0-9a-z]+")) {
+            serverWebExchange.getResponse()
+                    .getHeaders().add("Cache-Control", "max-age=3600");
+        }
+        return webFilterChain.filter(serverWebExchange);
+
+    }
+}
