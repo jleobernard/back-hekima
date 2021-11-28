c8e8b7c24d5367845fb3f788553ed862a8dc9e7c
junior bernard <junior.bernard-ext@alantaya.com>
2021-11-28 18:05:40 +0100

    Changement du serveur

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,10 +3,10 @@ set -e
 FINAL_DIR=/opt/containers/back-hekima
 mvn clean install package -DskipTests
 rc=$?; if [[ $rc != 0 ]]; then echo "Compilation error"; exit $rc; fi
-ssh ks-leo-noport "mkdir -p $FINAL_DIR"
-scp -r deploy/validation/* ks-leo-noport:$FINAL_DIR
-scp target/hekima.jar ks-leo-noport:$FINAL_DIR/docker
-ssh ks-leo-noport "cd $FINAL_DIR && docker-compose down"
+ssh mandela "mkdir -p $FINAL_DIR"
+scp -r deploy/validation/* mandela:$FINAL_DIR
+scp target/hekima.jar mandela:$FINAL_DIR/docker
+ssh mandela "cd $FINAL_DIR && docker-compose down"
 rc=$?; if [[ $rc != 0 ]]; then echo "Cannot stop container"; fi
-ssh ks-leo-noport "cd $FINAL_DIR && docker-compose up -d --build"
+ssh mandela "cd $FINAL_DIR && docker-compose up -d --build"
 rc=$?; if [[ $rc != 0 ]]; then echo "Cannot build container"; exit $rc; fi
