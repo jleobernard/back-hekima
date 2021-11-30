51f01de102d0bff3df59f61d572120c9eb9236db
junior bernard <junior.bernard-ext@alantaya.com>
2021-11-30 08:42:35 +0100

    - wip - verbes

--- src/main/java/com/leo/hekima/subs/SearchPattern.java
+++ src/main/java/com/leo/hekima/subs/SearchPattern.java
@@ -62,19 +62,23 @@ public class SearchPattern {
                                       final Multimap<PosTag, IndexEntry> index,
                                       final float minSimilarity) {
         final Map<Integer, Integer> countBySentenceInCorpus = new HashMap<>();
-        for (PosTag posTag : analyzedQuery) {
+        List<PosTag> fixParts = analyzedQuery.stream().filter(SearchPattern::isFix).collect(Collectors.toList());
+        if(fixParts.isEmpty()){
+            fixParts = analyzedQuery;
+        }
+        for (PosTag posTag : fixParts) {
             for (IndexEntry indexEntry : index.get(posTag)) {
                 countBySentenceInCorpus.compute(indexEntry.sentenceIndex(), (k,v) -> v == null ? 1 : v + 1);
             }
         }
-        final int minScore = (int)Math.floor(analyzedQuery.size() * minSimilarity);
+        final int minScore = (int)Math.floor(fixParts.size() * minSimilarity);
         return countBySentenceInCorpus.entrySet().stream().filter(e -> e.getValue() >= minScore)
             .sorted((e1, e2) -> e2.getValue() - e1.getValue())
             .map(Map.Entry::getKey)
             .collect(Collectors.toList());
     }
 
-    private record SavePoint(int positionInQuery, int lastPositionInCandidate, float score, int start){}
+    private record SavePoint(int positionInQuery, int lastPositionInCandidate, float score, int start, boolean hasStarted){}
 
     public static List<IndexWithScoreAndZone> scoreSentencesAgainstQuery(
             List<PosTag> analyzedQuery, List<Integer> candidates,
@@ -84,14 +88,15 @@ public class SearchPattern {
             // 1. they should all be in the same order
             // 2. we lose points according to PENALTIES
             final Stack<SavePoint> savePoints = new Stack<>();
-            SavePoint bestScore = new SavePoint(-1, -1, Integer.MIN_VALUE, -1);
-            savePoints.push(new SavePoint(0, -1, 0f, -1));
+            SavePoint bestScore = new SavePoint(-1, -1, Integer.MIN_VALUE, -1, false);
+            savePoints.push(new SavePoint(0, -1, 0f, -1, false));
             while(!savePoints.empty()) {
                 final SavePoint savePoint = savePoints.pop();
                 float score = savePoint.score;
                 int lastPositionInCandidate = savePoint.lastPositionInCandidate;
                 int start = savePoint.start;
                 int positionInQuery = savePoint.positionInQuery;
+                boolean hasStarted = savePoint.hasStarted;
                 for (int i = lastPositionInCandidate + 1; positionInQuery < analyzedQuery.size(); positionInQuery++) {
                     PosTag queryLemme = analyzedQuery.get(positionInQuery);
                     boolean found = false;
@@ -102,8 +107,12 @@ public class SearchPattern {
                             // We will explore this possibility with a bad alignment and move on to the next lemme
                             // BUT we save the failure for next time if there is something else to analyse
                             if(positionInQuery < analyzedQuery.size() - 1) {
-                                savePoints.push(new SavePoint(positionInQuery + 1, lastPositionInCandidate, score - PENALTIES.get(isFix(queryLemme) ? MISSING_FIX_WORD : MISSING_NF_WORD), start));
+                                savePoints.push(new SavePoint(positionInQuery + 1, lastPositionInCandidate, score - PENALTIES.get(isFix(queryLemme) ? MISSING_FIX_WORD : MISSING_NF_WORD), start, true));
+                                if(!hasStarted) {
+                                    savePoints.push(new SavePoint(positionInQuery + 1, positionInQuery, 0, start, false));
+                                }
                             }
+                            hasStarted = false;
                             AlignmentFailure failure = maybeFailure.get();
                             if (failure.equals(WRONG_VALUE_GOOD_TAG) || failure.equals(GOOD_VALUE_WRONG_TAG)) {
                                 score -= PENALTIES.getOrDefault(maybeFailure.get(), 0f);
@@ -123,14 +132,17 @@ public class SearchPattern {
                             }
                         } else {
                             found = true;
-                            for (int j = lastPositionInCandidate + 1; j < i; j++) {
-                                PosTag extra = candidate.get(j);
-                                if (isFix(extra)) {
-                                    score -= PENALTIES.get(EXTRA_FIX_WORD);
-                                } else {
-                                    score -= PENALTIES.get(EXTRA_NF_WORD);
+                            if(hasStarted) {
+                                for (int j = lastPositionInCandidate + 1; j < i; j++) {
+                                    PosTag extra = candidate.get(j);
+                                    if (isFix(extra)) {
+                                        score -= PENALTIES.get(EXTRA_FIX_WORD);
+                                    } else {
+                                        score -= PENALTIES.get(EXTRA_NF_WORD);
+                                    }
                                 }
                             }
+                            hasStarted = true;
                             lastPositionInCandidate = i;
                             if (start == -1) {
                                 start = i;
@@ -144,7 +156,7 @@ public class SearchPattern {
                     }
                 }
                 if(score > bestScore.score) {
-                    bestScore = new SavePoint(-1, lastPositionInCandidate, score, start);
+                    bestScore = new SavePoint(-1, lastPositionInCandidate, score, start, true);
                 }
             }
             return new IndexWithScoreAndZone(candidateIndex, bestScore.score, bestScore.start, bestScore.lastPositionInCandidate);
