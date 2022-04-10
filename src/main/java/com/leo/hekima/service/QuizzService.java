82b62789e1dba6c27268694c1a476814d29eda9c
junior bernard <junior.bernard-ext@alantaya.com>
2022-04-10 16:22:29 +0200

    Retrait connectionFactory pour générer les quizz

--- src/main/java/com/leo/hekima/service/QuizzService.java
+++ src/main/java/com/leo/hekima/service/QuizzService.java
@@ -7,10 +7,9 @@ import com.leo.hekima.repository.NoteRepository;
 import com.leo.hekima.to.AckResponse;
 import com.leo.hekima.to.ElementSummaryView;
 import com.leo.hekima.to.QuizzAnswerRequest;
-import com.leo.hekima.utils.ReactiveUtils;
 import com.leo.hekima.utils.WebUtils;
-import io.r2dbc.spi.ConnectionFactory;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -33,73 +32,71 @@ import static org.springframework.web.reactive.function.BodyExtractors.toMono;
 public class QuizzService {
     private final NoteQuizzHistoryRepository noteQuizzHistoryRepository;
     private final NoteRepository noteRepository;
-    private final ConnectionFactory connectionFactory;
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
 
     public QuizzService(NoteQuizzHistoryRepository noteQuizzHistoryRepository,
-                        NoteRepository noteRepository, ConnectionFactory connectionFactory) {
+                        NoteRepository noteRepository, R2dbcEntityTemplate r2dbcEntityTemplate) {
         this.noteQuizzHistoryRepository = noteQuizzHistoryRepository;
         this.noteRepository = noteRepository;
-        this.connectionFactory = connectionFactory;
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
     }
 
     public record NoteAndHistory(NoteSummary note, NoteQuizzHistoryModel history){}
 
     public Mono<ServerResponse> generate(ServerRequest serverRequest) {
-        return Mono.from(connectionFactory.create()).flatMap(connection -> {
-            final String baseRequest = """
-            SELECT DISTINCT ON (n.id) n.id as noteid, n.uri as noteuri, n.created_at as createdat, n.valeur as valeur FROM note n
-            LEFT JOIN note_tag nt ON n.id = nt.note_id
-            LEFT JOIN tag t ON nt.tag_id = t.id
-            LEFT JOIN note_source s ON n.source_id = s.id
-            """;
-            final Set<String> tags = getStringSet(serverRequest, "tags");
-            final Set<String> notTags = getStringSet(serverRequest, "notTags");
-            final Set<String> sources = getStringSet(serverRequest, "sources");
-            final int count = getCount(serverRequest);
-            final List<String> conditions = new ArrayList<>(3);
-            if(isNotEmpty(tags)) {
-                conditions.add(" t.uri IN ('" + String.join("','", tags) + "')");
-            }
-            if(isNotEmpty(notTags)) {
-                conditions.add("""
-                        n.id NOT IN (SELECT distinct(_nt.note_id) FROM note_tag _nt LEFT JOIN tag _t ON _nt.tag_id = _t.id
-                                                            WHERE _t.uri IN ('""" + String.join("','", notTags) + "'))");
-            }
-            if(isNotEmpty(sources)) {
-                conditions.add("s.uri IN ('" + String.join("','", sources) + "')");
-            }
-            final String request = baseRequest + (isEmpty(conditions) ? "" : " WHERE ") + String.join(" AND ", conditions) + " ORDER BY n.id";
-            final Flux<NoteSummary> allNotes = Flux.from(connection.createStatement(request).execute())
-                    .doFinally((st) -> Mono.from(connection.close()).subscribe())
-                    .take(count)
-                    .flatMap(result -> result.map((row, meta) -> new NoteSummary(
-                            row.get("noteid", Long.class),
-                            row.get("noteuri", String.class),
-                            row.get("createdat", Instant.class),
-                            row.get("valeur", String.class)
-                    )));
-            final Flux<ElementSummaryView> notesAndHistory = allNotes.flatMap(n -> optionalEmptyDeferred(noteQuizzHistoryRepository.findLastByNoteId(n.noteid()))
-                    .map(histo -> new NoteAndHistory(n, histo.orElse(null))))
-                    // Sort by last asked date
-                    // If one of the note was never asked then it takes precedence
-                    // If both were never asked then the oldest one takes precedence
-                    .sort((nah1, nah2) -> {
-                        if (nah1.history == null) {
-                            if (nah2.history == null) {
-                                return nah2.note.createdat().compareTo(nah1.note.createdat());
-                            } else {
-                                return -1;
-                            }
-                        } else if (nah2.history == null) {
-                            return 1;
+        final String baseRequest = """
+        SELECT DISTINCT ON (n.id) n.id as noteid, n.uri as noteuri, n.created_at as createdat, n.valeur as valeur FROM note n
+        LEFT JOIN note_tag nt ON n.id = nt.note_id
+        LEFT JOIN tag t ON nt.tag_id = t.id
+        LEFT JOIN note_source s ON n.source_id = s.id
+        """;
+        final Set<String> tags = getStringSet(serverRequest, "tags");
+        final Set<String> notTags = getStringSet(serverRequest, "notTags");
+        final Set<String> sources = getStringSet(serverRequest, "sources");
+        final int count = getCount(serverRequest);
+        final List<String> conditions = new ArrayList<>(3);
+        if(isNotEmpty(tags)) {
+            conditions.add(" t.uri IN ('" + String.join("','", tags) + "')");
+        }
+        if(isNotEmpty(notTags)) {
+            conditions.add("""
+                    n.id NOT IN (SELECT distinct(_nt.note_id) FROM note_tag _nt LEFT JOIN tag _t ON _nt.tag_id = _t.id
+                                                        WHERE _t.uri IN ('""" + String.join("','", notTags) + "'))");
+        }
+        if(isNotEmpty(sources)) {
+            conditions.add("s.uri IN ('" + String.join("','", sources) + "')");
+        }
+        final String request = baseRequest + (isEmpty(conditions) ? "" : " WHERE ") + String.join(" AND ", conditions) + " ORDER BY n.id";
+        final Flux<NoteSummary> allNotes = r2dbcEntityTemplate.getDatabaseClient().sql(request)
+            .map(row -> new NoteSummary(
+                    row.get("noteid", Long.class),
+                    row.get("noteuri", String.class),
+                    row.get("createdat", Instant.class),
+                    row.get("valeur", String.class)
+            ))
+            .all()
+            .take(count);
+        final Flux<ElementSummaryView> notesAndHistory = allNotes.flatMap(n -> optionalEmptyDeferred(noteQuizzHistoryRepository.findLastByNoteId(n.noteid()))
+                .map(histo -> new NoteAndHistory(n, histo.orElse(null))))
+                // Sort by last asked date
+                // If one of the note was never asked then it takes precedence
+                // If both were never asked then the oldest one takes precedence
+                .sort((nah1, nah2) -> {
+                    if (nah1.history == null) {
+                        if (nah2.history == null) {
+                            return nah2.note.createdat().compareTo(nah1.note.createdat());
                         } else {
-                            return nah1.history.getCreatedAt().compareTo(nah2.history.getCreatedAt());
+                            return -1;
                         }
-                    })
-                    .take(count)
-                    .map(n -> new ElementSummaryView(n.note.noteuri(), StringUtils.substring(n.note.valeur(), 0, 20)));
-            return ReactiveUtils.orEmptyList(notesAndHistory);
-        }).flatMap(notesAndHistory -> WebUtils.ok().bodyValue(notesAndHistory));
+                    } else if (nah2.history == null) {
+                        return 1;
+                    } else {
+                        return nah1.history.getCreatedAt().compareTo(nah2.history.getCreatedAt());
+                    }
+                })
+                .take(count)
+                .map(n -> new ElementSummaryView(n.note.noteuri(), StringUtils.substring(n.note.valeur(), 0, 20)));
+        return WebUtils.ok().body(notesAndHistory, ElementSummaryView.class);
     }
 
     public Mono<ServerResponse> answer(ServerRequest serverRequest) {
