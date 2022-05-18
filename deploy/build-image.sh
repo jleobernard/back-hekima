19230b93af52c0a732e5a5274ae84f96e183a7ec
Junior Léo Bernard <jleobernard@gmail.com>
2022-05-18 22:33:59 +0200

    Modification du pom et du script pour récupérer directement du pom la version du jar

--- deploy/build-image.sh
+++ deploy/build-image.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-NOTES_VERSION=2.0.2
+NOTES_VERSION=`grep '<!--MY_VERSION-->'  pom.xml  | sed -E 's/.*<version>//' |  sed -E 's/<\/version>.*//' | cut -f 1 -d '-'`
+
 FULL_PATH_TO_SCRIPT="$(realpath "$0")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 PROJECT_BASE="$(realpath "$SCRIPT_DIRECTORY/..")"
